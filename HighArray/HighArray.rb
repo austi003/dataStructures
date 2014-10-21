da6712d0cc8caa94f88e7d0663c36affe66e45d0
Shaun Austin <shaun.austin@careerbuilder.com>
2014-10-21 10:07:17 -0400

    Initialize HighArray constructor

--- HighArray/HighArray.rb
+++ HighArray/HighArray.rb
@@ -1,31 +1,20 @@
 
 class HighArray
-	def HighArray
-		arr = Array.new
+	def initialize
+		@arr = Array.new
 	end
 	
-	def find(searchKey)
-		arr.each do |x|
-			if x === searchKey
-				puts "this is the searchKey"
-			else 
-				puts "this is not"
-			end
-		end
-	end	
-	
-	def insert(value)
-		new_array.push(value)
+	def values
+		puts "This array is defined"
 	end
-
-	
 end
 
+my_array = HighArray.new
+your_array = HighArray.new
 
-my_array = Array.new    #I want to access my HighArray method here, but I'm apparently using this the wrong way for Ruby; can you give me a hint?
+my_array.values
+your_array.values
 
-my_array[0] = 10
 
-puts my_array[0]
 
 
