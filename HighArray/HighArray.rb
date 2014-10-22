a51f16e6fdd2c9c9f07db26219a8e048b408bd3d
Shaun Austin <shaun.austin@careerbuilder.com>
2014-10-22 07:09:02 -0400

    Add insert & display methods

--- HighArray/HighArray.rb
+++ HighArray/HighArray.rb
@@ -2,18 +2,27 @@
 class HighArray
 	def initialize
 		@arr = Array.new
+		@nElems = 0
 	end
 	
-	def values
-		puts "This array is defined"
+	def insert(value)
+		@arr[@nElems] = value
+		@nElems += 1
 	end
+		
+	def display
+		puts "There are #{@nElems} elements in your array: #{@arr}"
+	end
+
 end
 
 my_array = HighArray.new
 your_array = HighArray.new
 
-my_array.values
-your_array.values
+my_array.insert(10)
+my_array.insert(28)
+my_array.display
+
 
 
 
