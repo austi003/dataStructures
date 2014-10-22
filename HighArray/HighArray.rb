ecbf091841c965ef90353d2b4416ddecdb02843d
Shaun Austin <shaun.austin@careerbuilder.com>
2014-10-22 07:30:20 -0400

    begin building find method

--- HighArray/HighArray.rb
+++ HighArray/HighArray.rb
@@ -5,7 +5,7 @@ class HighArray
 		@nElems = 0
 	end
 	
-	def insert(value)
+	def add_element(value)
 		@arr[@nElems] = value
 		@nElems += 1
 	end
@@ -14,13 +14,22 @@ class HighArray
 		puts "There are #{@nElems} elements in your array: #{@arr}"
 	end
 
+=begin
+	def find(searchKey)
+		found = false
+			while found = false do
+				
+	end
+=end
+
 end
 
 my_array = HighArray.new
 your_array = HighArray.new
 
-my_array.insert(10)
-my_array.insert(28)
+my_array.add_element(10)
+my_array.add_element(28)
+my_array.add_element(85)
 my_array.display
 
 
